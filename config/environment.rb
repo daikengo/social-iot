3e8304372d86d7a2e8eef4f2c388f0c61ba3517f
daikengo <accheddu@gmail.com>
2014-07-21 17:45:02 +0200

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,17 +4,17 @@ require File.expand_path('../application', __FILE__)
 Siot::Application.configure do
 	config.action_controller.perform_caching = true
 	config.cache_store = :file_store, "#{Rails.root}/tmp/cache"
-  	config.action_mailer.default_url_options = { :host => 'siot.diee.unica.it' }
+  	config.action_mailer.default_url_options = { :host => 'server' }
 	config.action_mailer.delivery_method = :smtp
 	config.action_mailer.smtp_settings = {
     		:tls => true,
     		:openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,
-		    :address => 'smtp.diee.unica.it',
+		    :address => 'smtp.server',
 		    :port => 465,
     		:authentication => :login,
-		    :domain => 'siot.diee.unica.it',
-		    :user_name => 'siot@diee.unica.it',
-		    :password => 'ST:pr0jt',
+		    :domain => 'server',
+		    :user_name => 'user',
+		    :password => 'password',
         :enable_starttls_auto => false
 	}
 end
